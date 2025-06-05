194aa894c768d24586b3486257fd95710ca82628
cher-yami <chermanfreefire@gmail.com>
2025-06-04 21:44:42 -0500

    Add files via upload

--- ms-pagos/src/main/java/com/ccstudent/mspagos/entity/TipoPago.java
+++ ms-pagos/src/main/java/com/ccstudent/mspagos/entity/TipoPago.java
@@ -3,11 +3,11 @@ package com.ccstudent.mspagos.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id; 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @Entity
 @Data
