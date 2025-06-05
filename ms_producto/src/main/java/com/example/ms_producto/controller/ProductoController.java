a1171e30c256d8f26fa9138a3af23a044fd5d997
cher-yami <chermanfreefire@gmail.com>
2025-06-04 23:27:34 -0500

    Add files via upload

--- ms_producto/src/main/java/com/example/ms_producto/controller/ProductoController.java
+++ ms_producto/src/main/java/com/example/ms_producto/controller/ProductoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/productos")
+@RequestMapping("/productos")
 public class ProductoController {
 
     private final ProductoService productoService;
